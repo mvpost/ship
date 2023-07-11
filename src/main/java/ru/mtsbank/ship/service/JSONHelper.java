01d671c0f63ddb267b88a8e1bbf82832e62736ed
mvpostnikov <mvpost@bk.tu>
2023-07-11 19:29:19 +0300

    fix

--- src/main/java/ru/mtsbank/ship/service/JSONHelper.java
+++ src/main/java/ru/mtsbank/ship/service/JSONHelper.java
@@ -18,9 +18,18 @@ import java.util.Properties;
 public class JSONHelper {
     private final JsonMapper jsonMapper = new JsonMapper();
     private final OkHttpClient client = new OkHttpClient();
-    private static final String BASE_URL = getProperty("host") + ":" + getProperty("port");
-    private static final MediaType MEDIA_TYPE = MediaType
-            .parse(getProperty("mediaType"));
+    private static final MediaType MEDIA_TYPE;
+    private static final String BASE_URL;
+
+    static {
+        try {
+            MEDIA_TYPE = MediaType
+                    .parse(getProperty("mediaType"));
+            BASE_URL = getProperty("host") + ":" + getProperty("port");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
     private String getInitRequestJSON (String name, String type) throws JsonProcessingException {
         UrlRequest urlRequest = new UrlRequest();
@@ -31,10 +40,6 @@ public class JSONHelper {
 
     protected static String getBaseUrl() {
         return BASE_URL;
-    }
-
-    protected static MediaType getMediaType() {
-        return MEDIA_TYPE;
     }
 
     @NotNull
@@ -61,16 +66,14 @@ public class JSONHelper {
                 .getLocation();
     }
 
-    private static String getProperty(String propertyName) {
-        String propertyValue = "";
-        try {
-            File configFile = new File("src/main/resources/application.properties");
-            FileReader reader = new FileReader(configFile);
-            Properties props = new Properties();
-            props.load(reader);
-            propertyValue = props.getProperty(propertyName);
-            reader.close();
-        } catch (IOException ex) {}
+    private static String getProperty(String propertyName) throws IOException {
+        String propertyValue;
+        File configFile = new File("src/main/resources/application.properties");
+        FileReader reader = new FileReader(configFile);
+        Properties props = new Properties();
+        props.load(reader);
+        propertyValue = props.getProperty(propertyName);
+        reader.close();
         return propertyValue;
     }
 
